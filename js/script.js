796e4bdf6d49f42bb35ed3842686ce8089e5341f
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-05-13 15:04:31 +0400

    Initial commit :tada:

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// import "./window/window.js";
+// import "./scripts/scripts.js";
+// import "./libraries/libraries.js";
+
+/******/ })()
+;
