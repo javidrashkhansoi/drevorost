bdeb93b3c60e0884e29c5b743ea4ec76a100bb34
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-05-14 03:07:29 +0400

    Remove javascript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
-// import "./window/window.js";
-// import "./scripts/scripts.js";
-// import "./libraries/libraries.js";
 
 /******/ })()
 ;
